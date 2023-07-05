c99b7c62c907deae6edb0da47cf4dcfdb4876e7c
amirovstar <furkanza@code.edu.az>
2023-07-06 02:40:40 +0400

    task done

---
+++ BizLandApp/BizLandApp/Program.cs
@@ -0,0 +1,16 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+app.UseStaticFiles();
+
+app.MapControllerRoute(
+    "default",
+    "{controller=Home}/{action=Index}"
+    );
+
+
+app.Run();
+
